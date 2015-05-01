f7d41c77e7170142c6edcfc721b1a4e4f2802adc
Justin Finch <justin.finch@neofirma.com>
2015-05-01 09:36:22 -0500

    Implemented async methods for data provider

--- Hummingbird.EntityFramework/DataProvider.cs
+++ Hummingbird.EntityFramework/DataProvider.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Hummingbird.Data;
 
 namespace Hummingbird.EntityFramework
@@ -28,7 +29,20 @@ namespace Hummingbird.EntityFramework
 
         public IEnumerable<T> Find(Expression<Func<T, bool>> query, params Expression<Func<T, object>>[] includes)
         {
-            IQueryable<T> results = _context.Set<T>().Where(query); 
+            try
+            {
+                return FindAsync(query, includes).Result;
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+            
+        }
+
+        public async Task<List<T>> FindAsync(Expression<Func<T, bool>> query, params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> results = _context.Set<T>().Where(query);
 
             if (includes != null && includes.Length > 0)
             {
@@ -36,17 +50,29 @@ namespace Hummingbird.EntityFramework
                     (current, include) => current.Include(include));
             }
 
-            return results.ToList();
+            return await results.ToListAsync();
         }
 
         public IPagedResults<T> FindPage<TSortKey>(IPagedRequest<T, TSortKey> pagedRequest, params Expression<Func<T, object>>[] includes)
         {
-            if(pagedRequest.KeySelector == null)
+            try
+            {
+                return FindPageAsync(pagedRequest, includes).Result;
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+        }
+
+        public async Task<IPagedResults<T>> FindPageAsync<TSortKey>(IPagedRequest<T, TSortKey> pagedRequest, params Expression<Func<T, object>>[] includes)
+        {
+            if (pagedRequest.KeySelector == null)
                 throw new ArgumentException("Order By must be set for a paged result set.  Call the OrderBy method on the PagedRequest to set.");
 
             //Build the simple query
             IQueryable<T> query = _context.Set<T>().Where(pagedRequest.QueryExpression);
-            
+
             if (pagedRequest.Comparer != null)
             {
                 switch (pagedRequest.SortDirection)
@@ -83,7 +109,9 @@ namespace Hummingbird.EntityFramework
                 query = includes.Aggregate(query, (current, include) => current.Include(include));
             }
 
-            return new PagedResults<T>(query.ToList(), pagedRequest.PageSize, pagedRequest.PageNumber, totalCount);
+            var results = await query.ToListAsync();
+
+            return new PagedResults<T>(results, pagedRequest.PageSize, pagedRequest.PageNumber, totalCount);
         }
 
         public IQueryable<T> Query(params Expression<Func<T, object>>[] includes)
@@ -105,9 +133,30 @@ namespace Hummingbird.EntityFramework
             _context.SaveChanges();
         }
 
+        public async Task<bool> DeleteAsync(T item)
+        {
+            _context.Set<T>().Remove(item);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
         public void Delete(Expression<Func<T, bool>> query)
         {
             Find(query).ToList().ForEach(Delete);
+        }
+
+        public async Task<bool> DeleteAsync(Expression<Func<T, bool>> query)
+        {
+            var result = true;
+            var itemsToDelete = await FindAsync(query);
+
+            foreach (var itemToDelete in itemsToDelete)
+            {
+                result = await DeleteAsync(itemToDelete);
+            }
+
+            return result;
         }
 
         public T Save(T item)
@@ -115,6 +164,17 @@ namespace Hummingbird.EntityFramework
             _context.Set<T>().Add(item);
             _context.ApplyStateChanges();
             _context.SaveChanges();
+            _context.ResetStateChanges();
+            return item;
+        }
+
+        public async Task<T> SaveAsync(T item)
+        {
+            _context.Set<T>().Add(item);
+            _context.ApplyStateChanges();
+
+            await _context.SaveChangesAsync();
+            
             _context.ResetStateChanges();
             return item;
         }
@@ -132,24 +192,62 @@ namespace Hummingbird.EntityFramework
             return items;
         }
 
+        public async Task<IEnumerable<T>> SaveAsync(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                _context.Set<T>().Add(item);
+            }
+            _context.ApplyStateChanges();
+
+            await _context.SaveChangesAsync();
+
+            _context.ResetStateChanges();
+            return items;
+        }
+
         public IEnumerable<T> Execute(string sprocName, object parameters)
         {
+            try
+            {
+                return ExecuteAsync(sprocName, parameters).Result;
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+        }
+
+        public async Task<IEnumerable<T>> ExecuteAsync(string sprocName, object parameters)
+        {
             var arguments = parameters.PrepareSprocArguments(sprocName);
-            return _context.Set<T>().SqlQuery(arguments.Item1, arguments.Item2).ToList();
+            return await _context.Set<T>().SqlQuery(arguments.Item1, arguments.Item2).ToListAsync();
         }
 
         public void ExecuteNonQuery(string sprocName, object parameters)
         {
+            try
+            {
+                var result = ExecuteNonQueryAsync(sprocName, parameters).Result;
+            }
+            catch (AggregateException ae)
+            {
+                throw ae.Flatten();
+            }
+        }
+
+        public async Task<bool> ExecuteNonQueryAsync(string sprocName, object parameters)
+        {
             var arguments = parameters.PrepareSprocArguments(sprocName);
-            _context.Database.ExecuteSqlCommand(arguments.Item1, arguments.Item2);
+            await _context.Database.ExecuteSqlCommandAsync(arguments.Item1, arguments.Item2);
+
+            return true;
         }
 
         public void Dispose()
         {
             _context.Dispose();
         }
-
-
         
     }
 }
--- Hummingbird/Data/IDataProvider.cs
+++ Hummingbird/Data/IDataProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace Hummingbird.Data
 {
@@ -16,5 +17,14 @@ namespace Hummingbird.Data
         IEnumerable<T> Save(IEnumerable<T> items);
         IEnumerable<T> Execute(string sprocName, object parameters);
         void ExecuteNonQuery(string sprocName, object parameters);
+
+        Task<List<T>> FindAsync(Expression<Func<T, bool>> query, params Expression<Func<T, object>>[] includes);
+        Task<IPagedResults<T>> FindPageAsync<TSortKey>(IPagedRequest<T, TSortKey> pagedRequest, params Expression<Func<T, object>>[] includes);
+        Task<bool> DeleteAsync(T item);
+        Task<bool> DeleteAsync(Expression<Func<T, bool>> query);
+        Task<T> SaveAsync(T item);
+        Task<IEnumerable<T>> SaveAsync(IEnumerable<T> items);
+        Task<IEnumerable<T>> ExecuteAsync(string sprocName, object parameters);
+        Task<bool> ExecuteNonQueryAsync(string sprocName, object parameters);
     }
 }
